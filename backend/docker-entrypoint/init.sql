92f45d31c90be3feec80d120fe908510999d725a
richardMattsson <mattsson.richard@gmail.com>
2025-11-16 21:27:01 +0100

    remove encoding

--- backend/docker-entrypoint/init.sql
+++ backend/docker-entrypoint/init.sql
@@ -71,11 +71,11 @@ VALUES
 
 INSERT INTO testQuestionPhone(question)
 VALUES
-('Presenterade sig personen som någon från en bank, myndighet eller ett företag — utan att du själv kontaktat dem först?'),
+('Presenterade sig personen som någon från en bank, myndighet eller ett företag utan att du själv kontaktat dem först?'),
 ('Bad personen dig att lämna personliga uppgifter, kortnummer, BankID eller annan känslig information under samtalet?'),
 ('Lät personen stressad, hotfull eller försökte skapa panik för att få dig att agera snabbt (t.ex. "någon försöker ta dina pengar just nu!")?'),
-('Sa personen att du behövde ladda ner en app, logga in på din bank eller dela en kod för att ”lösa ett problem”?'),
-('Verkade numret misstänkt — t.ex. dolt, ovanligt långt eller liknade ett svenskt nummer men inte exakt (ex. +46701 i stället för 0701)?');
+('Sa personen att du behövde ladda ner en app, logga in på din bank eller dela en kod för att lösa ett problem?'),
+('Verkade numret misstänkt t.ex. dolt, ovanligt långt eller liknade ett svenskt nummer men inte exakt (ex. +46701 i stället för 0701)?');
 
 INSERT INTO users (username, password) VALUES ('Jane Doe', 'password'), ('Adam Pålsson', '1234'), ('Hugo Larsson', 'secret');
 
@@ -83,9 +83,13 @@ INSERT INTO reportedPhoneNumbers (phone_number, freetext) VALUES ('077-8137813',
 
 INSERT INTO reportedLinks (link, freetext) VALUES ('www.scam.com', 'internetbedrägeri');
 
-INSERT INTO testResults (user_id, suspect_details, result) VALUES ( 1, '077-8137813', 13);
+INSERT INTO testResults (user_id, suspect_details, result) VALUES (1, '077-8137813', 13);
 
-INSERT INTO wallPosts (free_text, phone_number, severity) VALUES ('Svara inte på detta numret!!!', '077-8137813', 'red' ), ('Ringer hela tiden', '071-121212', 'yellow'), ('Påstår sig komma från Kivra', '08-121212', 'red'), ('Påstår sig komma från Kivra nr2', '08-121212', 'orange');
+INSERT INTO wallPosts (free_text, phone_number, severity) VALUES
+('Svara inte på detta numret!!!', '077-8137813', 'red'),
+('Ringer hela tiden', '071-121212', 'yellow'),
+('Påstår sig komma från Kivra', '08-121212', 'red'),
+('Påstår sig komma från Kivra nr2', '08-121212', 'orange');
 
 CREATE INDEX testResults_user_id ON testResults (user_id);
 
